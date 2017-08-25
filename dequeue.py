20e5d9a9aad04596d0fe08318eb39e1199fdf224
Muh. Angga Muttaqien <angga.muttaqien@ruma.co.id>
2017-08-25 09:24:04 +0700

    add dequeu concept

--- dequeue.py
+++ dequeue.py
@@ -1,16 +1,25 @@
-class Queue:
+class Dequeue:
 
     def __init__(self):
         self.items = []
-    
+
     def is_empty(self):
         return self.items == []
 
-    def enqueue(self, item):
+    def add_front(self, item):
+        self.items.append(item)
+
+    def add_rear(self, item):
         self.items.insert(0, item)
 
-    def dequeue(self):
+    def remove_front(self):
         return self.items.pop()
+
+    def remove_rear(self):
+        return self.items.pop(0)
+        
+    def peek(self):
+        return self.items[len(self.items)-1]
 
     def size(self):
         return len(self.items)
@@ -18,19 +27,19 @@ class Queue:
     def get_list(self):
         return self.items
 
-print("=== QUEUE - First In First Out")
+print("=== DEQUEUE - First In First Out for Both Side")
 data = input("[INT, 0 to exit] What is your data? ")
-q = Queue()
+dq = Dequeue()
 while data != 0:
-    q.enqueue(data)
-    print(q.get_list())
+    dq.add_front(data)
+    print(dq.get_list())
     data = input('[INT, 0 to exit] What is your data? ')
 
 data = raw_input("[Y/N] Remove data: ")
 data = data.upper()
 while data !=  'N':
-    q.dequeue()
-    print(q.get_list())
+    dq.remove_front()
+    print(dq.get_list())
     data = raw_input('[[Y/N] Remove data: ')
 
 print('Thank you!')
