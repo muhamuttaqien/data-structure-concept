ada276d1367a4ea6cc4c8425d84021c7b6991665
Muh. Angga Muttaqien <angga.muttaqien@ruma.co.id>
2017-08-26 17:29:45 +0700

    add graph concept

---
+++ graph.py
@@ -0,0 +1,10 @@
+class AdjacencyList(object):
+
+    def __init__(self):
+        self.List = {}
+
+def main():
+    print("=== Graph - An abstract data type that is meant to implement the undirected graph and directed graph concepts consisting of a finite set of vertices or nodes or points")
+
+if __name__=='__main__':
+	main()
